c84d782aa633e174eb5503ed3c7a153290c7578f
gabi-pinheiro <gabitigre2011@gmail.com>
2022-08-27 08:50:31 -0300

    página de contato

--- resources/views/contato.blade.php
+++ resources/views/contato.blade.php
@@ -1,19 +1,23 @@
 @extends('template.default')
 @section('content')
 
+<div class="overlay-container"></div>
+<div class="overlay"></div>
+<div class="form">
+    <h1> Entre em contato com a gente</h1>
+    <form> 
+        <input type="text" name="" id="" placeholder="Nome Completo">
+        <br>
+        <input type="email" name="" id="" placeholder="E-mail para resposta">
+        <br>
+        <input type="tel" name="" id="" placeholder="Telefone">
+        <br>
+        <textarea name="" id="" cols="30" rows="10"  placeholder="Escreva sua mensagem"></textarea>
+        <br>
+        <input type="submit" value="Enviar">
+    </form>
+</div>
 
-<h1> Entre em contato com a gente</h1>
-<form> 
-    <input type="text" name="" id="" placeholder="Nome Completo">
-    <br>
-    <input type="email" name="" id="" placeholder="E-mail para resposta">
-    <br>
-    <input type="tel" name="" id="" placeholder="Telefone">
-    <br>
-    <textarea name="" id="" cols="30" rows="10"  placeholder="Escreva sua mensagem"></textarea>
-    <br>
-    <input type="submit" value="Enviar">
-</form>
 
 
 @endsection
