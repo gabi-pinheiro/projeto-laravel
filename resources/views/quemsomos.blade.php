6ae88f12b3b2727a6dcb71ab606baa2fc8f9c35b
gabi-pinheiro <gabitigre2011@gmail.com>
2022-08-26 22:00:40 -0300

    Atualizações diversas

--- resources/views/quemsomos.blade.php
+++ resources/views/quemsomos.blade.php
@@ -1,24 +1,36 @@
 @extends('template.default')
 @section('content')
 
-<div class="contato-content">
+<div class="quemsomos-content">
    <h1> Quem somos? </h1>
-    <div class="card">
+    <div id="card-gabi">
         <h3>Gabriela</h3>
         <img src="{{url('img/gabriela.jpeg')}}" >
         <p>
             Uma verdadeira "alma de velha", ama tudo o que é vintage. Ópera, vestidos e 
             história são suas paixões. Sempre lendo livros antigos e querendo conversar sobre 
             os assuntos mais aleatórios, descobriu que os brechós são ambientes perfeitos para isso.
+            <br><br>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eleifend, velit ut feugiat volutpat, dui nisi gravida tortor, vitae pellentesque felis ligula sed mi. Donec luctus tellus sed ligula faucibus, sed eleifend elit pulvinar. Donec dui elit, placerat ac viverra pharetra, ornare a diam. Nullam egestas suscipit lorem eu laoreet. Duis vitae leo felis. Cras dapibus felis nec augue suscipit porttitor. Vivamus sit amet mauris a nisi rhoncus dapibus id in lectus. Donec a porttitor nunc. Maecenas lacinia lectus non lectus tincidunt elementum. Nulla facilisi. Donec ullamcorper lacinia augue, ac varius felis commodo in. In hac habitasse platea dictumst.
+
+Etiam nec semper nibh, eget laoreet orci. Vivamus egestas suscipit fringilla. Vivamus bibendum diam eros, auctor facilisis dolor pellentesque ut. Proin lorem ligula, pulvinar sed pulvinar nec, placerat sed libero. Fusce quis mi accumsan, tristique erat ut, sollicitudin elit. Nam viverra accumsan nibh, vitae hendrerit mi placerat at. Praesent rhoncus sed elit eu venenatis. Nullam sagittis leo ullamcorper convallis luctus. Cras non metus tincidunt, mollis leo feugiat, imperdiet augue.
+
+
         </p>
     </div>
-    <div class="card">
+    <div id="card-lui" class="card">
         <h3>Lui</h3>
         <img src="{{url('img/lui.jpeg')}}" >
         <p>
             Roupas escuras, delineador, coturno e hobbies maravilhosos. Fã de cartografia, gosta de 
             usar seus dotes artísticos para desenhar mapas e criar mundos fictícios cheios de vida. 
             Se quiser conversar sobre My Chemical Romance, ele é a pessoa certa!
+            <br><br>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eleifend, velit ut feugiat volutpat, dui nisi gravida tortor, vitae pellentesque felis ligula sed mi. Donec luctus tellus sed ligula faucibus, sed eleifend elit pulvinar. Donec dui elit, placerat ac viverra pharetra, ornare a diam. Nullam egestas suscipit lorem eu laoreet. Duis vitae leo felis. Cras dapibus felis nec augue suscipit porttitor. Vivamus sit amet mauris a nisi rhoncus dapibus id in lectus. Donec a porttitor nunc. Maecenas lacinia lectus non lectus tincidunt elementum. Nulla facilisi. Donec ullamcorper lacinia augue, ac varius felis commodo in. In hac habitasse platea dictumst.
+
+Etiam nec semper nibh, eget laoreet orci. Vivamus egestas suscipit fringilla. Vivamus bibendum diam eros, auctor facilisis dolor pellentesque ut. Proin lorem ligula, pulvinar sed pulvinar nec, placerat sed libero. Fusce quis mi accumsan, tristique erat ut, sollicitudin elit. Nam viverra accumsan nibh, vitae hendrerit mi placerat at. Praesent rhoncus sed elit eu venenatis. Nullam sagittis leo ullamcorper convallis luctus. Cras non metus tincidunt, mollis leo feugiat, imperdiet augue.
+
+
         </p>
     </div>
 </div>
